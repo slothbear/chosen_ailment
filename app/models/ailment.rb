39d7b195b8d65d7e93291bcceaad80f5ea958618
Paul Morganthall <paul@morganthall.com>
2013-11-12 09:00:22 -0500

    Move Ailment making to Ailment.

--- app/models/ailment.rb
+++ app/models/ailment.rb
@@ -12,4 +12,23 @@ class Ailment < ActiveRecord::Base
     choices
   end
 
+  def self.find_or_create(aid)
+    if integer?(aid)
+      Ailment.find_by_id(aid)
+    elsif aid.present?
+      Ailment.create(:name => aid, :category => aid)
+    else
+      Ailment.new
+    end
+  end
+
+  private
+
+  # TODO: [THANKS] To Sarah Mei [1].
+  def self.integer?(value)
+    value.to_i.to_s == value
+  end
+
 end
+
+# [1] http://stackoverflow.com/questions/1235863/test-if-a-string-is-basically-an-integer-in-quotes-using-ruby
--- app/models/prac.rb
+++ app/models/prac.rb
@@ -1,27 +1,11 @@
 class Prac < ActiveRecord::Base
   belongs_to :ailment
-  # accepts_nested_attributes_for :ailment
   attr_accessible :ailment_attributes, :name, :ailment, :category, :ailment_id
 
   validates_presence_of :name
 
   def ailment_id=(aid)
-    self.ailment = if integer?(aid)
-      Ailment.find_by_id(aid)
-    elsif aid.present?
-      Ailment.create(:name => aid)
-    else
-      Ailment.new
-    end
-  end
-
-  private
-
-  # TODO: [THANKS] To Sarah Mei [1].
-  def integer?(value)
-    value.to_i.to_s == value
+    self.ailment = Ailment.find_or_create(aid)
   end
 
 end
-
-# [1] http://stackoverflow.com/questions/1235863/test-if-a-string-is-basically-an-integer-in-quotes-using-ruby

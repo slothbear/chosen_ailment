9e5239402b814a1853412fb07674526504a360ba
Paul Morganthall <paul@morganthall.com>
2013-11-12 10:16:07 -0500

    rename variables
    add comments
    tidy sections

--- app/models/ailment.rb
+++ app/models/ailment.rb
@@ -1,12 +1,19 @@
 class Ailment < ActiveRecord::Base
 
-  def self.find_or_create(aid)
-    if integer?(aid)
-      Ailment.find_by_id(aid)
-    elsif aid.present?
-      Ailment.find_or_create_by_name(aid)
+  # Select menus via Koenpunt/Chosen [1] allows user to select
+  # an existing Ailment or type in a new ailment name.
+  #   *  Existing ailments are represented by integers.
+  #   *  A new ailment name is passed as a string.
+  #   *  If no choice is made, a blank string is passed.
+
+  def self.find_or_create(new_id)
+    if integer?(new_id)
+      Ailment.find_by_id(new_id)
+    elsif new_id.present?
+      Ailment.find_or_create_by_name(new_id)
     else
-      Ailment.new
+      nil  # nil is returned automatically if the if/elsif isn't matched.
+           # make this condition explicit for now
     end
   end
 
@@ -16,11 +23,13 @@ class Ailment < ActiveRecord::Base
 
   private
 
-  # TODO: [THANKS] To Sarah Mei [1].
+  # THANKS: Sarah Mei [2]
   def self.integer?(value)
     value.to_i.to_s == value
   end
 
 end
 
-# [1] http://stackoverflow.com/questions/1235863/test-if-a-string-is-basically-an-integer-in-quotes-using-ruby
+# [1] https://github.com/harvesthq/chosen/pull/166
+
+# [2] http://stackoverflow.com/questions/1235863/test-if-a-string-is-basically-an-integer-in-quotes-using-ruby
--- app/models/prac.rb
+++ app/models/prac.rb
@@ -1,11 +1,11 @@
 class Prac < ActiveRecord::Base
+
   belongs_to :ailment
   attr_accessible :name, :ailment_id
-
   validates :name, :presence => true
 
-  def ailment_id=(aid)
-    self.ailment = Ailment.find_or_create(aid)
+  def ailment_id=(id_or_name)
+    self.ailment = Ailment.find_or_create(id_or_name)
   end
 
 end
